30a7d4575be01302ec20dcfdb400cabda8104264
@Helen853 <dudarevalen_1997@mail.ru>
2024-03-28 10:13:10 +0500

    Initial Commit

---
+++ Tests/AutorizationTests/AutorizationTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Autorization
+
+final class AutorizationTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        //XCTAssertEqual(Autorization().text, "Hello, World!")
+    }
+}
