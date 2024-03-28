668f8b226a57e40bd8f4b45143c86896ac8466c9
@Helen853 <dudarevalen_1997@mail.ru>
2024-03-28 17:51:54 +0500

    commit

--- Sources/Autorization/Autorization.swift
+++ Sources/Autorization/Autorization.swift
@@ -1,9 +1,7 @@
 import UIKit
 
 public class AutorizationTextField: UITextField {
-    public func setupPlaceholder(title: String) -> UITextField {
-        let textField = UITextField()
-        textField.placeholder = title
-        return textField
+    public func setupPlaceholder(title: String) {
+        self.placeholder = title
     }
 }
