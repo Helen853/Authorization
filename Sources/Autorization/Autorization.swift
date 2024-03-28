d1ad6f61096100429c4726ed99e265c41d453d17
@Helen853 <dudarevalen_1997@mail.ru>
2024-03-28 17:19:21 +0500

    Добавила настройку плейсхолдера

--- Sources/Autorization/Autorization.swift
+++ Sources/Autorization/Autorization.swift
@@ -1,5 +1,8 @@
 import UIKit
 
 public class AutorizationTextField: UITextField {
-    
+    func setupPlaceholder(title: String) -> UITextField {
+        let textField = UITextField()
+        textField.placeholder = title
+    }
 }
